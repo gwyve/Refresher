ac38d936ef5b64395821c32787099d61d62ca9e4
ve <359818086@qq.com>
2017-06-09 15:54:12 +0800

    add 034

--- src/OJ/LeetCode/Num034SearchForARange.java
+++ src/OJ/LeetCode/Num034SearchForARange.java
@@ -18,10 +18,46 @@ public class Num034SearchForARange {
         }
         return ret;
     }
+    public int[] searchRange1(int[] nums,int target){
+//        这个是先二分发搜索
+        int[] ret = new int[]{-1,-1};
+        if (nums == null || nums.length == 0)
+            return ret;
+        int start =  0;
+        int end = nums.length-1;
+        int mid = -1;
+        while(start <= end){
+            mid = (start + end) /2;
+            if (target == nums[mid])
+                break;
+            if (target < nums[mid]){
+                end = mid -1;
+            }else {
+                start = mid +1;
+            }
+        }
+        if (nums[mid] != target)
+            return ret;
+        start = mid;
+        end = mid;
+        while(start > -1){
+            if (nums[start] != target){
+                break;
+            }
+            start --;
+        }
+        while (end < nums.length ){
+            if (nums[end] != target){
+                break;
+            }
+            end ++;
+        }
+        return  new int[]{start+1,end-1};
+    }
 
     public static  void main(String[] args){
         Num034SearchForARange obj = new Num034SearchForARange();
-        int[] ret = obj.searchRange(new int[]{8,8},8);
+        int[] ret = obj.searchRange1(new int[]{8},8);
         for (int i = 0; i < ret.length; i++) {
             System.out.println(ret[i]);
         }
