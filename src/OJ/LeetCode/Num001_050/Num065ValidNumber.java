2f0dbddeb639d486b5117150a77739f9370e62d8
VE <359818086@qq.com>
2019-05-09 21:44:32 +0800

    Num065

---
+++ src/OJ/LeetCode/Num001_050/Num065ValidNumber.java
@@ -0,0 +1,50 @@
+package OJ.LeetCode.Num001_050;
+
+import javax.xml.parsers.FactoryConfigurationError;
+
+public class Num065ValidNumber {
+    public boolean isNumber(String s){
+        int[][] trans =  new int[][]{
+                {-1, 0, 1, 2, 3,-1},
+                {-1,-1,-1, 2, 3,-1},
+                {-1, 4,-1, 2, 6, 5},
+                {-1,-1,-1, 6,-1,-1},
+                {-1, 4,-1,-1,-1,-1},
+                {-1,-1, 7, 8,-1,-1},
+                {-1, 4,-1, 6,-1, 5},
+                {-1,-1,-1, 8,-1,-1},
+                {-1, 4,-1, 8,-1,-1}
+        };
+        int state = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int type;
+            if (s.charAt(i)>='0' && s.charAt(i)<='9'){
+                type = 3;
+            }else if (s.charAt(i) == ' '){
+                type = 1;
+            }else if (s.charAt(i) == '+' || s.charAt(i) == '-'){
+                type = 2;
+            }else if (s.charAt(i) == 'E' || s.charAt(i) == 'e'){
+                type = 5;
+            }else if (s.charAt(i) == '.'){
+                type = 4;
+            }else {
+                type = 0;
+            }
+            state = trans[state][type];
+            if (state == -1)
+                return false;
+        }
+        return state == 2 || state == 4 || state == 6 || state == 8;
+
+
+    }
+
+
+
+    public static void main(String[] args){
+        Num065ValidNumber obj = new Num065ValidNumber();
+
+        System.out.println(obj.isNumber(" 005047e+6"));
+    }
+}
