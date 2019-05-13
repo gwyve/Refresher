e654ad1bbe62e9c6455519fe983ba080f5ef9358
VE <359818086@qq.com>
2019-05-13 16:21:12 +0800

    num084

--- src/OJ/LeetCode/Num051_100/Num084LargestRectangleinHistogram.java
+++ src/OJ/LeetCode/Num051_100/Num084LargestRectangleinHistogram.java
@@ -2,8 +2,32 @@ package OJ.LeetCode.Num051_100;
 
 public class Num084LargestRectangleinHistogram {
 
+
+
+
+    public int largestRectangleArea1(int[] heights){
+        // 这个则会比这钱的复杂度小一下，主要在于，如果是递增的直接继续这个，这个主要是按照竖着来找的
+        int res = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (i+1<heights.length && heights[i] < heights[i+1]){
+                continue;
+            }
+            int minH = heights[i];
+            for (int j = i; j >=0 ; j--) {
+                minH = minH<heights[j]?minH:heights[j];
+                int area = minH*(i-j+1);
+                res = area>res?area:res;
+            }
+
+        }
+        return res;
+    }
+
+
+
     public int largestRectangleArea(int[] heights) {
         // 我这个的做法，确实是个比较笨的方法，虽然能够ac，但是笨n^2的复杂度吧
+        // 这个的做法是划横线来操作，怎么做都是n^2的复杂度
          int res = 0;
         for (int i = 0; i < heights.length; i++) {
             int length = helper(heights,heights[i]);
@@ -32,6 +56,6 @@ public class Num084LargestRectangleinHistogram {
 
     public static void main(String[] args){
         Num084LargestRectangleinHistogram obj = new Num084LargestRectangleinHistogram();
-        System.out.println(obj.largestRectangleArea(new int[]{2,1,5,6,2,3}));
+        System.out.println(obj.largestRectangleArea1(new int[]{2, 1, 5, 6, 2, 3}));
     }
 }
