116a866366fbb1ad5c0985361d5dc7c1bb7ac417
VE <359818086@qq.com>
2019-05-21 16:41:26 +0800

    num121 扫描一遍

--- src/OJ/LeetCode/Num101_150/Num121BestTimetoBuyandSellStock.java
+++ src/OJ/LeetCode/Num101_150/Num121BestTimetoBuyandSellStock.java
@@ -3,6 +3,7 @@ package OJ.LeetCode.Num101_150;
 public class Num121BestTimetoBuyandSellStock {
     public int maxProfit(int[] prices) {
         // 最动脑子的方法，速度特别慢
+        // 这个是需要扫面两边的n平方级别的
         int res = Integer.MIN_VALUE;
         for (int i = 0; i < prices.length; i++) {
             int ru = prices[i];
@@ -16,4 +17,17 @@ public class Num121BestTimetoBuyandSellStock {
         else
             return 0;
     }
+    public int maxProfit1(int[] prices){
+        // 因为卖出一定是在买入之后，所以只用扫描一遍就好
+        if (prices==null || prices.length==0)
+            return 0;
+        int buy = Integer.MAX_VALUE;
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            buy = buy<prices[i]?buy:prices[i];
+            res = res>(prices[i]-buy)?res:prices[i]-buy;
+        }
+        return res;
+    }
+
 }
