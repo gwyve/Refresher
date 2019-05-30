87037fd05991f7d46fbd9077d48bfa0898c38dd3
VE <359818086@qq.com>
2019-05-30 15:36:16 +0800

    num144

---
+++ src/OJ/LeetCode/Num101_150/Num144BinaryTreePreorderTraversal.java
@@ -0,0 +1,28 @@
+package OJ.LeetCode.Num101_150;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Num144BinaryTreePreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        helper(root,res);
+        return res;
+    }
+    public void helper(TreeNode root,List<Integer> res) {
+        if (root == null)
+            return;
+        res.add(root.val);
+        helper(root.left,res);
+        helper(root.right,res);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
