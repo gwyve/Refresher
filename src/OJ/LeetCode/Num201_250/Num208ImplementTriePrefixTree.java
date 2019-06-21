066f5dde8708e46690d52cd32961db7baf3a71bc
VE <359818086@qq.com>
2019-06-21 16:40:42 +0800

    num208

---
+++ src/OJ/LeetCode/Num201_250/Num208ImplementTriePrefixTree.java
@@ -0,0 +1,63 @@
+package OJ.LeetCode.Num201_250;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Num208ImplementTriePrefixTree {
+
+    private class TrieNode{
+        TrieNode[] child = new TrieNode[26];
+        boolean isWord;
+    }
+
+    TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Num208ImplementTriePrefixTree() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode run = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            int tmp = word.charAt(i) - 'a';
+            if (run.child[tmp] == null){
+                run.child[tmp] = new TrieNode();
+            }
+            run = run.child[tmp];
+            System.out.println("sss");
+        }
+        run.isWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode run = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            int tmp = word.charAt(i) - 'a';
+            run = run.child[tmp];
+            if (run == null)
+                return false;
+        }
+        return run.isWord;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode run = this.root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int tmp = prefix.charAt(i) - 'a';
+            run = run.child[tmp];
+            if (run == null)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Num208ImplementTriePrefixTree obj = new Num208ImplementTriePrefixTree();
+        obj.insert("apple");
+        System.out.println(obj.search("app"));
+        System.out.println(obj.startsWith("app"));
+    }
+}
